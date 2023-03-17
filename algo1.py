cbed63a0015bb291da47d99563e7334cc9692755
Joanna <89415649+Jocruiser@users.noreply.github.com>
2023-03-17 16:07:17 -0700

    Add files via upload

---
+++ algo1.py
@@ -0,0 +1,45 @@
+def target_cities(source_arrayofcities, target_arrayofcities):
+    source = source_arrayofcities[0]
+    output_order = []
+    output_array = []
+    index = 0
+
+    for start in source:
+        for target in target_arrayofcities:
+            if(len(source) - index < len(target)):
+                continue
+
+            substring = source[index:index+len(target)]
+            
+            if target == substring:
+                output_order.append(index)
+                output_array.append(target)
+        
+        index = index + 1
+
+    return output_order, output_array
+
+                
+def main():
+    list_index = []
+    index = 0
+
+    f = open("in2a.txt", "r")
+   
+    line = f.readline()
+    
+    while(line):
+
+        source_str = line.split('"')[1::2]
+    
+        line = f.readline()
+        target_str = line.split('"')[1::2]
+        
+        print(target_cities(source_str, target_str))
+        
+        line = f.readline()
+
+    f.close()
+
+if __name__ == "__main__":
+    main()
